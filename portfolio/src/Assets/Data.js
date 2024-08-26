17fe02b918b663d30a67cef90739d55416c7a3b4
Arshathkky <Arshathhaseen@gmail.com>
2024-08-26 15:36:28 +0530

    ready to build

--- portfolio/src/Assets/Data.js
+++ portfolio/src/Assets/Data.js
@@ -7,19 +7,20 @@ const projects = [
     image: 'https://private-user-images.githubusercontent.com/87074193/348888229-476dfbbc-93c9-4a2c-992e-595bd4b1e60d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjQ2NjMwOTEsIm5iZiI6MTcyNDY2Mjc5MSwicGF0aCI6Ii84NzA3NDE5My8zNDg4ODgyMjktNDc2ZGZiYmMtOTNjOS00YTJjLTk5MmUtNTk1YmQ0YjFlNjBkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA4MjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwODI2VDA4NTk1MVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTY2MmViY2Y0NjJhNTEyNGIyYmU0NDIwMWYxNTRhZDg4OTJiMDBlNGIxYThhOGRlZmQ0ZmYxNWU5NGViMTNmM2MmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.YF2mSjatbLro0XqwNxRkJCg0VWOEaiutWSvgcOsBfnc' 
   },
   {
-    title: 'Student Grading System',
-    description: 'A student grading system used to manage student grades and marks by lecturers. It includes a marking scheme, GPA calculation, and overall GPA analysis.',
-    technologies: 'HTML | CSS | JAVASCRIPT | MONGODB | Socket.io',
-    link: 'https://github.com/Arshathkky/StudentGradesystem',
-    image: '' 
-  },
-  {
     title: 'Hologram Quiz App',
     description: 'A hologram quiz application involving both hardware and software development. A hologram box was developed to display the questions, and a keyboard was assigned for answer selection. Questions are made by videos and mirrored into the hologram display.',
     technologies: 'HTML | CSS | JAVASCRIPT | MONGODB | CAPCUT',
     link: 'https://github.com/Arshathkky/quiz-app',
     image: 'https://github-production-user-asset-6210df.s3.amazonaws.com/87074193/361377486-24a553b2-ec4f-452f-b3da-834b2e7f793b.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240826%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240826T090644Z&X-Amz-Expires=300&X-Amz-Signature=e8ef6c0f59f047d9c57900e2374501e6c763f85fa1776d59cf74fb87a8c77759&X-Amz-SignedHeaders=host&actor_id=87074193&key_id=0&repo_id=802729553'
   },
+  {
+    title: 'Student Grading System',
+    description: 'A student grading system used to manage student grades and marks by lecturers. It includes a marking scheme, GPA calculation, and overall GPA analysis.',
+    technologies: 'HTML | CSS | JAVASCRIPT | MONGODB | Socket.io',
+    link: 'https://github.com/Arshathkky/StudentGradesystem',
+    image: '' 
+  },
+  
 ];
 
 export default projects;
